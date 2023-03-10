c0bf56b6f72843f5049d800ccecf442f6e59b980
NunoNartMatias <nunosilvamatias@gmail.com>
2023-03-10 01:27:52 +0000

    Added -- Operation and Printed the Results

--- ManipularVar/Program.cs
+++ ManipularVar/Program.cs
@@ -2,7 +2,7 @@
 
 namespace ManipularVar
 {
-    class Program
+    public class Program
     {   
         // Main Method
         static void Main(string[] args)
@@ -11,10 +11,20 @@ namespace ManipularVar
             // Declaring our variables
 
             int x = 4, z = 2;
+            int y, a;
 
             // Operation for ++
 
-            int y = x++;    
+            y = x++ + z++; // x = 5 | z = 2 | y = 6   
+
+            // Operation for --
+            a = --z + --x; // z = 1 | x = 4 | y = 5
+
+            // Printing First Result
+            Console.WriteLine("Result is " + y);
+
+            // Printing Second Result
+            Console.WriteLine("Result is " + a); 
 
 
         }
