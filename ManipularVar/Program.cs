34a62fdc16edb86bbbd003aa76ba959603a582df
NunoNartMatias <nunosilvamatias@gmail.com>
2023-03-10 01:01:46 +0000

    Added Variables and ++ Operation

---
+++ ManipularVar/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ManipularVar
+{
+    class Program
+    {   
+        // Main Method
+        static void Main(string[] args)
+
+        {
+            // Declaring our variables
+
+            int x = 4, z = 2;
+
+            // Operation for ++
+
+            int y = x++;    
+
+
+        }
+    }
+}
